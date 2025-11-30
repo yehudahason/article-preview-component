c6ab0789b1298099ec92f154d67edb4e1f823282
Yehuda Hason <yehudaxt@gmail.com>
2025-11-30 22:02:48 +0200

    js active3 commit

--- js/script.js
+++ js/script.js
@@ -2,26 +2,28 @@ const shareButton1 = document.getElementById("btn1");
 const shareButton2 = document.getElementById("btn2");
 const popUp = document.querySelector(".share-wrapper");
 const bottom = document.querySelector(".bottom");
-const shareBottom = document.querySelector(".share-bottom");
+const shareLinksBottom = document.querySelector(".share-links-bottom");
+const activeBtn = document.querySelector(".share-btn");
 
 window.addEventListener("resize", (e) => {
   if (window.innerWidth >= 768) {
     bottom.classList.remove("hide");
-    shareBottom.classList.add("hide");
+    shareLinksBottom.classList.add("hide");
     return;
   }
 });
 shareButton1.addEventListener("click", (e) => {
   if (window.innerWidth < 768) {
-    shareBottom.classList.toggle("hide");
+    shareLinksBottom.classList.toggle("hide");
     bottom.classList.toggle("hide");
     return;
   }
   popUp.classList.toggle("active");
+  activeBtn.classList.toggle("active");
 });
 
 shareButton2.addEventListener("click", (e) => {
-  shareBottom.classList.toggle("hide");
+  shareLinksBottom.classList.toggle("hide");
   bottom.classList.remove("hide");
   popUp.classList.remove("active");
 });
