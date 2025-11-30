d85c5e8d4173c28283906835cd6c5ee17f305728
Yehuda Hason <yehudaxt@gmail.com>
2025-11-30 21:17:38 +0200

    js active2 commit

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ shareButton1.addEventListener("click", (e) => {
   if (window.innerWidth < 768) {
     shareBottom.classList.toggle("hide");
     bottom.classList.toggle("hide");
+    return;
   }
   popUp.classList.toggle("active");
 });
