975b95d7ac428631b26ad511104c1fa36f83e854
Yehuda Hason <yehudaxt@gmail.com>
2025-11-30 22:13:03 +0200

    cleaning commit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-const shareButton1 = document.getElementById("btn1");
-const shareButton2 = document.getElementById("btn2");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
 const popUp = document.querySelector(".share-wrapper");
-const bottom = document.querySelector(".bottom");
-const shareLinksBottom = document.querySelector(".share-links-bottom");
+const mbottom = document.querySelector(".michelle-bottom");
+const slb = document.querySelector(".share-links-bottom");
 const activeBtn = document.querySelector(".share-btn");
 
 window.addEventListener("resize", (e) => {
   if (window.innerWidth >= 768) {
-    bottom.classList.remove("hide");
-    shareLinksBottom.classList.add("hide");
+    mbottom.classList.remove("hide");
+    slb.classList.add("hide");
     return;
   }
 });
-shareButton1.addEventListener("click", (e) => {
+btn1.addEventListener("click", (e) => {
   if (window.innerWidth < 768) {
-    shareLinksBottom.classList.toggle("hide");
-    bottom.classList.toggle("hide");
+    slb.classList.toggle("hide");
+    mbottom.classList.toggle("hide");
     return;
   }
   popUp.classList.toggle("active");
   activeBtn.classList.toggle("active");
 });
 
-shareButton2.addEventListener("click", (e) => {
-  shareLinksBottom.classList.toggle("hide");
-  bottom.classList.remove("hide");
+btn2.addEventListener("click", (e) => {
+  slb.classList.toggle("hide");
+  mbottom.classList.remove("hide");
   popUp.classList.remove("active");
 });
